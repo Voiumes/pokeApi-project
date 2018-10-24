1f78e27e93edfb37480160ec3b01129b024f4de1
Sean <omerta201@yahoo.com>
2018-10-24 02:35:17 -0400

    -

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const request = require('request');
 app.set('view engine', 'ejs');
+app.use(express.static("public"));
 app.get('/', function(req, res){
     res.render('search');
 })
@@ -17,7 +18,7 @@ app.get('/results', function(req,res){
     })
 });
 
-// app.listen(3000, function(){
-//     console.log('live port 3000')
-// });
-app.listen(process.env.PORT, process.env.IP);
+app.listen(3000, function(){
+    console.log('live port 3000')
+});
+// app.listen(process.env.PORT, process.env.IP);
