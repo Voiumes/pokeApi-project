5d724b2f83c3a5a8b646d7f7417df423684b7143
Sean <omerta201@yahoo.com>
2018-10-25 18:07:55 -0400

    -

--- app.js
+++ app.js
@@ -4,20 +4,20 @@ const express = require('express');
 const app = express();
 const request = require('request');
 app.set('view engine', 'ejs');
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.render('search');
 })
-app.get('/results', function(req,res){
+app.get('/results', function (req, res) {
     var pokemon = req.query.pokemon
-    request('https://pokeapi.co/api/v2/pokemon/' + pokemon, function(error,response, body){
-        if(!error && response.statusCode === 200){
+    request('https://pokeapi.co/api/v2/pokemon/' + pokemon, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
             let pokedata = JSON.parse(body)
-            res.render('results', {pokedata: pokedata})
+            res.render('results', { pokedata: pokedata })
         }
     })
 });
 
-// app.listen(3000, function(){
-//     console.log('live port 3000')
-// });
-app.listen(process.env.PORT, process.env.IP);
+app.listen(3000, function(){
+    console.log('live port 3000')
+});
+// app.listen(process.env.PORT, process.env.IP);
